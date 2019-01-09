957288ee705079f33b5293af75cfcbe83404ac35
Monika Maria <monika.kocot9@gmail.com>
2019-01-09 14:29:33 +0100

    sortListPeople_rev

---
+++ src/com/company/lambdy_sort/sortListPeople/Person.java
@@ -0,0 +1,36 @@
+package com.company.lambdy_sort.sortListPeople;
+
+public class Person implements Comparable<Person>{
+
+    private char gender;
+    public  String name;
+    private String surname;
+
+    public Person(char gender, String name, String surname) {
+        this.gender = gender;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "gender=" + gender +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        int compareSurname = surname.compareTo(o.surname);
+
+        if(compareSurname  == 0) {
+            return name.compareTo(o.name);
+        }
+        else {
+            return compareSurname ;
+        }
+    }
+
+}
---
+++ src/com/company/lambdy_sort/sortListPeople/PersonSort.java
@@ -0,0 +1,44 @@
+package com.company.lambdy_sort.sortListPeople;
+
+import java.util.*;
+
+public class PersonSort {
+
+    public static void main(String[] args) {
+
+        List<Person> people = new ArrayList<Person>();
+        people.add(new Person('K', "Asia", "Wczorajsza"));
+        people.add(new Person('M', "Marcin", "Nikczemny"));
+        people.add(new Person('M', "Slawek", "Abacki"));
+        people.add(new Person('K', "Kasia", "Borowik"));
+        people.add(new Person('K', "Dorota", "Borowik"));
+        people.add(new Person('M', "Tomek", "Daszek"));
+        people.add(new Person('K', "Ania", "Daszek"));
+
+        System.out.println("NOT SORTED: ");
+        for (Person person : people) {
+            System.out.println(person);
+        }
+        System.out.println("SORTED BY SURNAME: ");
+        Collections.sort(people);
+        for (Person person : people) {
+            System.out.println(person);
+        }
+
+        System.out.println("COMPARATOR NAME: ");
+        System.out.println("SORTED BY NAME ");
+        // sort with LAMBDA:
+        Collections.sort(people, (arg1, arg2) ->
+                {
+                    int name = (arg1.name).compareTo(arg2.name);
+                    if (name == 0) {
+                        return arg1.compareTo(arg2);
+                    }
+                    return name;
+                }
+        );
+
+        people.forEach((arg1) -> System.out.println(arg1));
+
+    }
+}
